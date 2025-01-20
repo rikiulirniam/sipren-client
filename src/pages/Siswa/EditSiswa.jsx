3ba3d4ba2394ff7205c04db0d60178b4cacf019b
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2025-01-20 09:22:56 +0700

    fe

--- src/pages/Siswa/EditSiswa.jsx
+++ src/pages/Siswa/EditSiswa.jsx
@@ -7,62 +7,63 @@ import { useAxios } from "../../utils/Provider";
 import Swal from "sweetalert2";
 
 export const EditSiswa = () => {
-  const { id } = useParams();
+  const { nis, id_kelas } = useParams();
   const axios = useAxios();
   const param = useParams();
   const [current, setCurrent] = useState({
-    tingkat: "X",
-    jurusan: 1,
+    rfid: "",
+    nama: "",
+    nis: "",
+    jenis_kelamin: 1,
+    kelas: 1,
   });
   const pathname = window.location.pathname;
+  const [kelas, setKelas] = useState([]);
 
   const handleChangeJurusan = (e) => {
     setCurrent({ ...current, jurusan: e.target.value });
   };
 
   const handleChangeTingkat = (e) => {
-    setCurrent({ ...current, tingkat: e.target.value });
+    setCurrent({ ...current, jenis_kelamin: parseInt(e.target.value) });
   };
 
   const handleSubmitForm = (e) => {
     e.preventDefault();
 
-    console.log({
-      id_jurusan: parseInt(current.jurusan),
-      tingkat: current.tingkat,
-      no_kelas: parseInt(current.no_kelas),
-    });
+    const payload = {
+      rfid: current.rfid,
+      nama: current.nama,
+      nis: current.nis,
+      jenis_kelamin: current.jenis_kelamin,
+      id_kelas: id_kelas,
+    };
+
     if (pathname.includes("/update")) {
       axios
-        .put(`/kelas/${id}`, {
-          id_jurusan: current.jurusan,
-          tingkat: current.tingkat,
-          no_kelas: current.no_kelas,
-        })
+        .put(`/siswa/${nis}`, payload)
         .then((res) => {
           window.location = param;
         })
         .catch((err) => {
           Swal.fire({
             title: "Error!",
-            text: err.response.data.message,
+            text: err.response?.data?.message || "Terjadi kesalahan",
             icon: "error",
             confirmButtonText: "Tutup",
           });
         });
     } else {
       axios
-        .get(
-          `/kelas?id_jurusan=${current.jurusan}&tingkat=${current.tingkat}&no_kelas=${current.no_kelas}`
-        )
+        .post(`/siswa`, payload)
         .then((res) => {
           window.location.reload();
         })
         .catch((err) => {
-          console.log("error");
+          console.log(payload);
           Swal.fire({
             title: "Error!",
-            text: err.response.data.message,
+            text: err.response?.data?.message || "Terjadi kesalahan",
             icon: "error",
             confirmButtonText: "Tutup",
           });
@@ -70,57 +71,31 @@ export const EditSiswa = () => {
     }
   };
 
-  function handleDeleteSiswa(e) {}
-
   useEffect(() => {
     if (pathname.includes("/update")) {
       axios
-        .get(`/kelas/detail?id_kelas=${id}`)
+        .get(`/siswa/${nis}`)
         .then((res) => {
-          console.log(res.data);
-          setKelas(res.data.data[0]);
-          setCurrent({
-            jurusan: res.data.data[0].id_jurusan,
-            tingkat: res.data.data[0].tingkat,
-            no_kelas: res.data.data[0].no_kelas,
-          });
+          setCurrent(res.data.data[0]);
         })
         .catch((err) => {
-          Swal.fire({
-            title: "Error!",
-            text: err.response.data.message,
-            icon: "error",
-            confirmButtonText: "Tutup",
-          }).then(() => {
-            window.location = "/kelas";
-          });
+          console.log(err);
         });
     }
 
     axios
-      .get(`/siswa?id_kelas=${id}`)
+      .get("/kelas")
       .then((res) => {
-        setSiswa(res.data.data);
-      })
-      .catch((err) => {
-        Swal.fire({
-          title: "Error!",
-          text: err.response.data.message,
-          icon: "error",
-          confirmButtonText: "Tutup",
-        });
-      });
-  }, [id]);
-
-  useEffect(() => {
-    axios
-      .get("/jurusan")
-      .then((res) => {
-        setJurusan(res.data.data);
-        console.log(res.data.data);
+        setKelas(res.data.data);
       })
       .catch((err) => {
         console.log(err);
+        Swal.fire({
+          title: "Error!",
+          text: err.response?.data?.message || "Terjadi kesalahan",
+          icon: "error",
+          confirmButtonText: "Tutup",
+        });
       });
   }, []);
 
@@ -128,122 +103,65 @@ export const EditSiswa = () => {
     <AuthGuard>
       <Navbar />
       <Sidebar />
-      <div className="main flex-col">
+      <div className="main">
         <form
           className="p-5 w-full bg-blue_dark text-white bg-opacity-90 rounded-lg"
           onSubmit={handleSubmitForm}
         >
           <h1 className="text-3xl font-bold pb-7">
-            {pathname.includes("/update") ? "Update " : "Add "} Kelas
+            {pathname.includes("/update") ? "Update " : "Add "} Siswa
           </h1>
 
-          <div className="content-form flex items-end gap-x-10">
-            <div className="flex flex-col ">
-              <label htmlFor="tingkat" className="p-2">
-                Tingkat:
-              </label>
-              <select
-                id="tingkat"
-                name="tingkat"
-                onChange={handleChangeTingkat}
-                value={current.tingkat}
-                className="text-blue_dark rounded p-2 px-3 w-24"
-              >
-                <option value="X">X</option>
-                <option value="XI">XI</option>
-                <option value="XII">XII</option>
-              </select>
-            </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="rfid" className="p-2">
+              RFID :
+            </label>
+            <input
+              type="text"
+              id="rfid"
+              name="rfid"
+              value={current.rfid || ""}
+              onChange={(e) => setCurrent({ ...current, rfid: e.target.value })}
+              className="text-blue_dark rounded p-2 px-3"
+            />
+          </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="nama" className="p-2">
+              Nama :
+            </label>
+            <input
+              type="text"
+              id="nama"
+              name="nama"
+              value={current.nama || ""}
+              onChange={(e) => setCurrent({ ...current, nama: e.target.value })}
+              className="text-blue_dark rounded p-2 px-3"
+            />
+          </div>
 
-            <div className="flex flex-col w-48">
-              <label htmlFor="jurusan" className="p-2 ">
-                Jurusan:
-              </label>
-              <select
-                id="jurusan"
-                name="jurusan"
-                onChange={handleChangeJurusan}
-                className="text-blue_dark rounded p-2 px-3"
-              >
-                {jurusan &&
-                  jurusan.map((jur, i) => (
-                    <option key={i} value={jur.id_jurusan}>
-                      {jur.akronim}
-                    </option>
-                  ))}
-              </select>
-            </div>
-
-            <div className="flex flex-col w-24">
-              <label htmlFor="no_kelas" className="p-2">
-                No Kelas:   
-              </label>
-              <input
-                type="number"
-                id="no_kelas"
-                name="no_kelas"
-                value={current.no_kelas}
-                onChange={(e) =>
-                  setCurrent({ ...current, no_kelas: e.target.value })
-                }
-                className="text-blue_dark rounded py-2 ps-3"
-              />
-            </div>
+          <div className="flex flex-col mb-5">
+            <label htmlFor="nis" className="p-2">
+              NIS :
+            </label>
+            <input
+              type="text"
+              id="nis"
+              name="nis"
+              value={current.nis || ""}
+              onChange={(e) => setCurrent({ ...current, nis: e.target.value })}
+              className="text-blue_dark rounded p-2 px-3"
+            />
           </div>
 
           <div className="flex float-end gap-3">
             <Link to="/kelas" className="p-4 py-2 rounded bg-red">
               Kembali
             </Link>
-            <button type="submit" className="px-4 py-2 rounded bg-blue">
+            <button type="submit" className="p-4 py-2 rounded bg-blue">
               Kirim
             </button>
           </div>
         </form>
-        <table className="w-full table">
-          <thead>
-            <tr>
-              <th>Nama</th>
-              <th>NIS</th>
-              <th>Kelas</th>
-              <th>RFID ID</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {siswa && siswa.length > 0 ? (
-              siswa.map((s, i) => (
-                <tr key={i}>
-                  <td>{s.nama}</td>
-                  <td>{s.nis}</td>
-                  <td>{`${s.tingkat} ${s.akronim} ${s.no_kelas}`}</td>
-                  <td>{s.rfid}</td>
-                  <td className="flex gap-2 justify-center">
-                    <a
-                      href={`/siswa/update/${s.nis}`}
-                      className="bg-orange_scale py-1 px-4 rounded"
-                    >
-                      Edit
-                    </a>
-                    <button
-                      onClick={handleDeleteSiswa}
-                      value={s.nis}
-                      className="bg-red py-1 px-4 rounded"
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={5} className="text-center">
-                  Belum ada siswa di kelas ini
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
       </div>
     </AuthGuard>
   );
